fe3897454d7aa9085d801721c221fc318f5c7994
Srinivasu <162099573+Srinivasu2000@users.noreply.github.com>
2026-05-16 15:29:28 +0530

    Delete variables.tf

--- variables.tf
+++
@@ -1,8 +0,0 @@
-variable "aws_region" {}
-variable "vpc_cidr" {}
-variable "subnet_cidr" {}
-variable "availability_zone" {}
-variable "ami_id" {}
-variable "instance_type" {}
-variable "public_key_path" {}
-variable "private_key_path" {}
